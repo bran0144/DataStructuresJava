aaa31feed26865610d779f3de03b3e33cd6578b7
bra0144 <bran0144@umn.edu>
2021-09-01 16:25:34 -0500

    added stack methods

--- src/main/java/BasicStack.java
+++ src/main/java/BasicStack.java
@@ -1,11 +1,48 @@
 public class BasicStack <X> {
-    private X [] data;
+    private X[] data;
+    private int pointer;
 
     public BasicStack() {
         data = (X[]) new Object[1000];
+        pointer = 0;
+    }
 
+    public void push(X newItem) {
+        data[pointer++] = newItem;
+    }
+
+    public X pop() {
+        if (pointer == 0) {
+            throw new IllegalStateException("no more items in stack");
+        }
+        return data[--pointer];
+    }
+
+    public boolean contains(X item) {
+        boolean found = false;
+        for (int i = 0; i < pointer; i++) {
+            if (data[i].equals(item)) {
+                found = true;
+                break;
+            }
+        }
+        return found;
+    }
+
+    public X access(X item) {
+        while (pointer > 0) {
+            X tempItem = pop();
+            if (item.equals(tempItem)) {
+                return tempItem;
+            }
+        }
+        throw new IllegalArgumentException("Could not find item in stack");
+    }
+    public int size() {
+        return pointer;
     }
 }
+
 
 
 // Stacks are LIFO (last in first out)
